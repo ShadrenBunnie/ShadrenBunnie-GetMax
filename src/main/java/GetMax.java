0ba0a4b1fbe507354f31a6b64fa18c272f585622
Brande H <brandelinh@gmail.com>
2023-03-03 05:59:45 +0000

    Find Max in Array

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,14 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = arr[0]; // initialize max
+
+        for(int i = 0; i < arr.length; i++){ //start to loop through the array
+            if( max < arr[i]){ // compare variable max to the array element
+                //if the element is greater than the previous max variable, then replace with new element to max variable
+            max = arr[i];                
+            }
+        }
+        return max; //print out the official number after cycling through the array
     }
 }
